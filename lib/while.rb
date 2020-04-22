0681a20618fd718ec83d9e0181577ab06531bc0a
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 01:05:31 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
-def using_while(levitation_force)
+def using_while
   levitation_force = 6
-  while num_of_hotdogs_eaten < 10
+  while levitation_force < 10
 	using_while +=1
   puts "Wingardium Leviosa"
 end
